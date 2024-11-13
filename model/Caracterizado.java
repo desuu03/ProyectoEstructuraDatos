78b44c1bee714b4cddf06a64beba34d17a9849ce
nosee8 <148650256+nosee8@users.noreply.github.com>
2024-11-13 17:41:55 -0500

    Base

---
+++ model/Caracterizado.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Caracterizado {
+    private String tipoDocumento;
+    private String documento;
+    private String nombre;
+    private String caracterizacion; // INHABILITAR o EMBARGAR
+
+    public Caracterizado(){}
+
+    public Caracterizado(String tipoDocumento, String documento,
+                         nombre, String caracterizacion) {
+        this.tipoDocumento = tipoDocumento;
+        this.documento = documento;
+        this.nombre = nombre;
+        this.caracterizacion = caracterizacion;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCaracterizacion() {
+        return caracterizacion;
+    }
+
+    public void setCaracterizacion(String caracterizacion) {
+        this.caracterizacion = caracterizacion;
+    }
+}
---
+++ model/Persona.java
@@ -0,0 +1,151 @@
+package model;
+public class Persona{
+    private String nombre;
+    private String cedula;
+    private int edad;
+    private String genero; //Masculino o Femenino
+    private String lugarNacimiento; //PAIS-DEPARTAMENTO-CIUDAD
+    private String lugarResidencia; //PAIS-DEPARTAMENTO-CIUDAD
+    private String institucionPublica; // si no es CIVIL
+    private boolean prepensionado;
+    private String entidadAnterior;
+    private boolean hijosINPEC;
+    private boolean condecorado;
+    private boolean familiaresPolicias;
+    private String observacionesDisciplinarias;
+    private String estado; //RECHAZADO APROBADO INHABILITADO EMBARGADO
+
+    public Persona(){}
+    public Persona(String nombre, String cedula, int edad, String genero,
+                   String lugarNacimiento, String lugarResidencia, String institucionPublica,
+                   boolean prepensionado, String entidadAnterior, boolean hijosINPEC,
+                   boolean condecorado, boolean familiaresPolicias,
+                   String observacionesDisciplinarias, String estado) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.genero = genero;
+        this.lugarNacimiento = lugarNacimiento;
+        this.lugarResidencia = lugarResidencia;
+        this.institucionPublica = institucionPublica;
+        this.prepensionado = prepensionado;
+        this.entidadAnterior = entidadAnterior;
+        this.hijosINPEC = hijosINPEC;
+        this.condecorado = condecorado;
+        this.familiaresPolicias = familiaresPolicias;
+        this.observacionesDisciplinarias = observacionesDisciplinarias;
+        this.estado=estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getLugarNacimiento() {
+        return lugarNacimiento;
+    }
+
+    public void setLugarNacimiento(String lugarNacimiento) {
+        this.lugarNacimiento = lugarNacimiento;
+    }
+
+    public String getLugarResidencia() {
+        return lugarResidencia;
+    }
+
+    public void setLugarResidencia(String lugarResidencia) {
+        this.lugarResidencia = lugarResidencia;
+    }
+
+    public String getInstitucionPublica() {
+        return institucionPublica;
+    }
+
+    public void setInstitucionPublica(String institucionPublica) {
+        this.institucionPublica = institucionPublica;
+    }
+
+    public boolean isPrepensionado() {
+        return prepensionado;
+    }
+
+    public void setPrepensionado(boolean prepensionado) {
+        this.prepensionado = prepensionado;
+    }
+
+    public String getEntidadAnterior() {
+        return entidadAnterior;
+    }
+
+    public void setEntidadAnterior(String entidadAnterior) {
+        this.entidadAnterior = entidadAnterior;
+    }
+
+    public boolean isHijosINPEC() {
+        return hijosINPEC;
+    }
+
+    public void setHijosINPEC(boolean hijosINPEC) {
+        this.hijosINPEC = hijosINPEC;
+    }
+
+    public boolean isCondecorado() {
+        return condecorado;
+    }
+
+    public void setCondecorado(boolean condecorado) {
+        this.condecorado = condecorado;
+    }
+
+    public boolean isFamiliaresPolicias() {
+        return familiaresPolicias;
+    }
+
+    public void setFamiliaresPolicias(boolean familiaresPolicias) {
+        this.familiaresPolicias = familiaresPolicias;
+    }
+
+    public String getObservacionesDisciplinarias() {
+        return observacionesDisciplinarias;
+    }
+
+    public void setObservacionesDisciplinarias(String observacionesDisciplinarias) {
+        this.observacionesDisciplinarias = observacionesDisciplinarias;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
